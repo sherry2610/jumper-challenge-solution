00af377e1da021dc450c5892f54b8fbf02164613
sherry2610 <shaharyarkhalid2610@gmail.com>
2025-03-07 08:23:23 +0100

    feat(frontend): Tokens Table and some general refactoring

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,11 @@
 import AccountConnection from '@/components/AccountCreation';
 import TokenList from '@/components/TokenList';
 import { Box, Container, Typography } from '@mui/material';
-import Image from 'next/image';
 
 export default function Home() {
   return (
     <Container maxWidth='md'>
       <Box sx={{ my: 4 }}>
-        <Box display='flex' justifyContent='center'>
-          <Typography variant='h1'>Welcome to Jumper challenge!</Typography>
-        </Box>
-
         <Typography variant='h3' align='center' gutterBottom>
           Wallet Auth and ERC20 Token Dashboard
         </Typography>
